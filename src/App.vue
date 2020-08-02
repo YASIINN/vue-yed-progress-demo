28ef70db5366082a9fa6078ca84b1470ba7ddfb1
ydalkilic <ysndlklc1234@gmail.com>
2020-08-02 16:38:14 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <vue-yed-progressbar :percent="70"/>
+        <br/>
+        <vue-yed-progressbar :percent="80" progressColor="colorPink"/>
+        <br/>
+        <vue-yed-progressbar :percent="40" progressColor="colorPurple" progressTextColor="black"/>
+        <br/>
+        <vue-yed-progressbar :percent="79" progressStripe="stripePurple" :progressStripeMode="true"/>
+        <br/>
+        <vue-yed-progressbar :percent="68" progressStripe="stripeGreen" :progressStripeMode="true"/>
+
+
+
+    </div>
+</template>
+<script>
+    import VueYedProgressbar from "vue-yed-progressbar"
+    export default {
+        name: 'App',
+        components: {
+            VueYedProgressbar
+        },
+    }
+</script>
+<style>
+    body{
+        padding: 40px;
+    }
+</style>
+
+
---
+++ src/components/progress/YedProgressBar.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="yedprogress" >
+        <div class="yedprogressbar" :class="progressStripeMode===false?progressColor:progressStripe" :style="{width:percent+'%'}">
+        </div>
+            <div class="yedprogresstext" :style="{color:progressTextColor}"><em> %{{percent}}</em></div>
+
+    </div>
+</template>
+
+<script>
+    import "./yedprogressbar.css"
+    export default {
+        name: "YedProgressBar",
+        props:{
+            progressColor:{
+                type:String,
+                required: false,
+                default:"colorBlue",
+                validator:function(value){
+                    return ['colorRed', 'colorOrange', 'colorYellow','colorGreen','colorPurple','colorPink','colorBlue'].indexOf(value) !== -1
+                }
+            },
+            progressStripeMode:{
+                type:Boolean,
+                required:false
+            },
+            progressStripe:{
+                type:String,
+                required:false,
+                default:"stripeBlue",
+                validator:function(value){
+                    return ['stripeGray', 'stripeGreen', 'stripeYellow','stripePurple','stripePink','stripeOrange','stripeRed','stripeBlue'].indexOf(value) !== -1
+                }
+            },
+            progressTextColor:{
+                type:String,
+                required:false,
+                default: "#fff"
+            },
+            percent:{
+                type:Number,
+                required:true
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+
+
+
+</style>
